12b7ef2e492c9be2a1d7a83275208daf08966093
Jordan Dunn <jordan@nodetwo.net>
2018-05-07 13:24:46 -0500

    More type hinting. I find that it helps me understand the modules im using when I have to look up the return types

--- mailer/admin_routes.py
+++ mailer/admin_routes.py
@@ -2,12 +2,15 @@ from functools import wraps
 from pprint import pprint
 
 from flask import Blueprint, flash, redirect, render_template, request
+from flask import Response
 from werkzeug.exceptions import BadRequestKeyError
 
 from mailer.models.customers import CustomerManager
 from mailer.models.queue import QueueManager
 from mailer.models.sessions import SessionManager
 from mailer.models.users import UserManager
+
+import typing
 
 admin_routes = Blueprint(__name__, 'admin_routes',
                          url_prefix='/admin')
@@ -17,7 +20,7 @@ def login_required(f):
     @wraps(f)
     def decorator(**kwargs):
         try:
-            args = request.args
+            args: typing.Dict(str, typing.Any) = request.args
             if SessionManager.validate(args['token']):
                 return f(**kwargs)
             else:
@@ -29,64 +32,64 @@ def login_required(f):
 
 @admin_routes.route('/', methods=['GET'])
 @login_required
-def index():
-    args = request.args
+def index() -> Response:
+    args: typing.Dict(str, typing.Any) = request.args
     return render_template('index.j2', title="Home", current_link="admin_home", token=args['token'], admin_view=True)
 
 
 @admin_routes.route('/users', methods=['GET'])
 @login_required
-def users():
-    args = request.args
+def users() -> Response:
+    args: typing.Dict(str, typing.Any) = request.args
     return render_template('users.j2', title="Users", current_link="users", token=args['token'], admin_view=True)
 
 
 @admin_routes.route('/users/add', methods=['GET'])
 @login_required
-def users_add():
-    args = request.args
+def users_add() -> Response:
+    args: typing.Dict(str, typing.Any) = request.args
     return render_template('users_add.j2', title="Add User", current_link="users", token=args['token'], admin_view=True)
 
 
 @admin_routes.route('/users/add/do', methods=['POST'])
 @login_required
-def users_add_do():
-    args = request.args
-    form = request.form
+def users_add_do() -> Response:
+    args: typing.Dict(str, typing.Any) = request.args
+    form: typing.Dict(str, typing.Any) = request.form
     return redirect('/users?token=' + args['token'])
 
 
 @admin_routes.route('/users/remove/<int:user_id>', methods=['GET'])
 @login_required
-def users_remove(user_id: int):
-    args = request.args
+def users_remove(user_id: int) -> Response:
+    args: typing.Dict(str, typing.Any) = request.args
     return redirect('/users?token=' + args['token'])
 
 
 @admin_routes.route('/franchises', methods=['GET'])
 @login_required
-def franchises():
-    args = request.args
+def franchises() -> Response:
+    args: typing.Dict(str, typing.Any) = request.args
     return render_template('franchises.j2', title="Franchises", current_link="franchises", token=args['token'], admin_view=True)
 
 
 @admin_routes.route('/franchises/add', methods=['GET'])
 @login_required
-def franchises_add():
-    args = request.args
+def franchises_add() -> Response:
+    args: typing.Dict(str, typing.Any) = request.args
     return render_template('franchises_add.j2', title="Add Franchise", current_link="franchises", token=args['token'], admin_view=True)
 
 
 @admin_routes.route('/franchises/add/do', methods=['POST'])
 @login_required
-def franchises_add_do():
-    args = request.args
-    form = request.form
+def franchises_add_do() -> Response:
+    args: typing.Dict(str, typing.Any) = request.args
+    form: typing.Dict(str, typing.Any) = request.form
     return redirect('/franchises?token=' + args['token'])
 
 
 @admin_routes.route('/franchises/remove/<int:franchise_id>', methods=['GET'])
 @login_required
-def franchises_remove(franchise_id: int):
-    args = request.args
+def franchises_remove(franchise_id: int) -> Response:
+    args: typing.Dict(str, typing.Any) = request.args
     return redirect('/franchises?token=' + args['token'])
--- mailer/routes.py
+++ mailer/routes.py
@@ -2,12 +2,14 @@ from functools import wraps
 from pprint import pprint
 
 from flask import Blueprint, flash, redirect, render_template, request
+from flask import Response
 from werkzeug.exceptions import BadRequestKeyError
 
 from mailer.models.customers import CustomerManager
 from mailer.models.queue import QueueManager
 from mailer.models.sessions import SessionManager
 from mailer.models.users import UserManager
+import typing
 
 app_routes = Blueprint(__name__, 'app_routes')
 
@@ -16,7 +18,7 @@ def login_required(f):
     @wraps(f)
     def decorator(**kwargs):
         try:
-            args = request.args
+            args: typing.Dict(str, typing.Any) = request.args
             if SessionManager.validate(args['token']):
                 return f(**kwargs)
             else:
@@ -28,24 +30,24 @@ def login_required(f):
 
 @app_routes.route('/', methods=['GET'])
 @login_required
-def index():
-    args = request.args
+def index() -> Response:
+    args: typing.Dict[str, typing.Any] = request.args
     return render_template('index.j2', title="Home", current_link="home", token=args['token'], admin_view=False)
 
 
 @app_routes.route('/login', methods=['GET'])
-def login():
+def login() -> Response:
     UserManager.first_run()
     return render_template('login.j2', title="Login", current_link="login")
 
 
 @app_routes.route('/login/do', methods=['POST'])
-def login_do():
-    form = request.form
+def login_do() -> Response:
+    form: typing.Dict(str, typing.Any) = request.form
     if form['username'] and form['password']:
         res = UserManager.validate(form['username'], form['password'])
         if res is not False:
-            return redirect('/?token='+str(res))
+            return redirect('/?token=' + str(res))
         else:
             flash('Invalid username and/or password', 'login_error')
             return redirect('/login')
@@ -56,30 +58,30 @@ def login_do():
 
 @app_routes.route('/logout', methods=['GET'])
 @login_required
-def logout():
+def logout() -> Response:
     return redirect('/login')
 
 
 @app_routes.route('/queue', methods=['GET'])
 @login_required
-def queue():
-    args = request.args
+def queue() -> Response:
+    args: typing.Dict(str, typing.Any) = request.args
     queue = QueueManager.get_queue()
     return render_template('queue.j2', title="Queue", current_link="queue", token=args['token'], queue=queue)
 
 
 @app_routes.route('/queue/add', methods=['GET'])
 @login_required
-def queue_add():
-    args = request.args
+def queue_add() -> Response:
+    args: typing.Dict(str, typing.Any) = request.args
     return render_template('queue_add.j2', title="Add Customer", current_link="queue", token=args['token'])
 
 
 @app_routes.route('/queue/add/do', methods=['POST'])
 @login_required
-def queue_add_do():
-    args = request.args
-    form = request.form
+def queue_add_do() -> Response:
+    args: typing.Dict(str, typing.Any) = request.args
+    form: typing.Dict(str, typing.Any) = request.form
     if CustomerManager.add_customer(form['first_name'], form['last_name'], form['email'], form['phone']):
         customer = CustomerManager.get_customer(form['email'])
         if QueueManager.add_queue(customer.customer_id):
@@ -94,8 +96,8 @@ def queue_add_do():
 
 @app_routes.route('/queue/remove/<int:queue_id>', methods=['GET'])
 @login_required
-def queue_remove(queue_id: int):
-    args = request.args
+def queue_remove(queue_id: int) -> Response:
+    args: typing.Dict(str, typing.Any) = request.args
     if QueueManager.remove_queue(queue_id):
         return redirect('/queue?token=' + args['token'])
     else:
@@ -105,43 +107,43 @@ def queue_remove(queue_id: int):
 
 @app_routes.route('/customers', methods=['GET'])
 @login_required
-def customers():
-    args = request.args
+def customers() -> Response:
+    args: typing.Dict(str, typing.Any) = request.args
     return render_template('customers.j2', title='Customers', token=args['token'], current_link='customers')
 
 
 @app_routes.route('/customers/remove/<int:customer_id>', methods=['GET'])
 @login_required
-def customers_remove(customer_id: int):
-    args = request.args
+def customers_remove(customer_id: int) -> Response:
+    args: typing.Dict(str, typing.Any) = request.args
     return redirect('/customers?token=' + args['token'])
 
 
 @app_routes.route('/customers/update/<int:customer_id>', methods=['GET'])
 @login_required
-def customers_update(customer_id: int):
-    args = request.args
-    form = request.form
+def customers_update(customer_id: int) -> Response:
+    args: typing.Dict(str, typing.Any) = request.args
+    form: typing.Dict(str, typing.Any) = request.form
     return redirect('/customers?token=' + args['token'])
 
 
 @app_routes.route('/templates', methods=['GET'])
 @login_required
-def templates():
-    args = request.args
+def templates() -> Response:
+    args: typing.Dict(str, typing.Any) = request.args
     return render_template('templates.j2', title='Templates', token=args['token'], current_link='templates')
 
 
 @app_routes.route('/templates/add', methods=['GET'])
 @login_required
-def templates_add():
-    args = request.args
+def templates_add() -> Response:
+    args: typing.Dict(str, typing.Any) = request.args
     return render_template('templates_add.j2', title='Add Template', token=args['token'], current_link='templates')
 
 
 @app_routes.route('/templates/add/do', methods=['POST'])
 @login_required
-def templates_add_do():
-    args = request.args
-    form = request.form
+def templates_add_do() -> Response:
+    args: typing.Dict(str, typing.Any) = request.args
+    form: typing.Dict(str, typing.Any) = request.form
     return redirect('/templates?token=' + args['token'])
