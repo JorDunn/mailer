7a94c47d548971b4c4a54a2008d7e1764909f9ce
Jordan Dunn <jordan@nodetwo.net>
2018-05-16 15:07:08 -0500

    Fixed email host & port issue

--- mailer/models/email.py
+++ mailer/models/email.py
@@ -1,4 +1,5 @@
 import smtplib
+from pathlib import Path
 
 from email.headerregistry import Address
 from email.mime.multipart import MIMEMultipart
@@ -22,30 +23,26 @@ class EmailManager(object):
     def construct_email(self, queue_id: int) -> None:
         customer, template = QueueManager.get_queue_item(queue_id)
         msg = MIMEMultipart()
-        html = """
-        <html>
-            <head></head>
-            <body>
-            {template_content}
-            </body>
-        </html>"""
+        with open(str(Path.cwd()) + '/mailer/mjml/base.html', 'r') as f:
+            html = f.read()
         html = html.replace('{template_content}', template.body)
         html = html.replace('{0}', customer.first_name)
         msg.attach(MIMEText(html, 'html'))
         self.receiver = customer.email
         msg['Subject'] = template.name.replace('{0}', customer.first_name)
+        msg['From'] = self.sender
+        msg['To'] = self.receiver
         self.body = msg
 
     def send(self, debuglevel: int=0) -> None:
         try:
             print("Trying to send an email to {}...".format(self.receiver), end='')
-            server = smtplib.SMTP_SSL(
-                Config.EMAIL['HOST'], Config.EMAIL['PORT'])
+            server = smtplib.SMTP_SSL(host=Config.EMAIL['host'], port=Config.EMAIL['port'])
             server.set_debuglevel(debuglevel)
             # If you are using gmail, this requires an app specific passwords
             server.login('{0}@{1}'.format(
                 Config.EMAIL['username'], Config.EMAIL['domain']), Config.EMAIL['password'])
-            server.sendmail(self.sender, self.receiver, self.body.as_string())
+            server.sendmail(from_addr=self.sender, to_addrs=self.receiver, msg=self.body.as_string())
             print("complete!")
         except Exception as err:
             print(err)
